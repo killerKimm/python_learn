8bf9dda8569c6334bb2418e32f3c3112ff6de21b
killerKimm <dasistbot@gmail.com>
2022-11-29 17:22:00 +0100

    11 x

--- dds.py
+++ dds.py
@@ -1,18 +1,17 @@
 import os
 # CLEAR THE CONSOL
-os.system("cls")
+
 
 # DEFAULT FIELD
 
 
 class Field:
 
-    I = [["X", ".", "."],
-         ["X", ".", "."],
-         ["X", ".", "."]]
+    I = [[".", ".", "."],
+         [".", ".", "."],
+         [".", ".", "."]]
 
     status = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
-   
 
     available_moves = {"q": [0, 0], "w": [0, 1], "e": [0, 2],
                        "a": [1, 0], "s": [1, 1], "d": [1, 2],
@@ -21,10 +20,6 @@ class Field:
     def __init__(self):
         self.fields = Field.I
 
-    # THE DICT OF AVEILABLE MOVES (Q,W,E,
-    #                             A,S,D,
-    #                             Z,X,C)
-    
     # DRAW THE FIELD
 
     @staticmethod
@@ -37,8 +32,8 @@ class Field:
     def update_field(self, val: str, player: str):
         fields = self.fields
         if val in self.available_moves:
-            key = self.available_moves[val] # [0, 0]
-            y = key[0] # 0
+            key = self.available_moves[val]  # [0, 0]
+            y = key[0]  # 0
 
             x = key[1]
 
@@ -46,12 +41,12 @@ class Field:
 
             fields[y][x] = player
             self.fields = fields
-            self.draw_field(fields)
 
             #i[key[0][0]][key[1][0]] = "X"
 
 
 class Player:
+
     def __init__(self, role: str, name: str):
         self.role = role
         self.name = name
@@ -63,29 +58,97 @@ class Player:
         # This is the player
 
 # Game controller
+
+
 class Game:
+
     def __init__(self, player1: Player, player2: Player):
         self.field = Field()
         self.player1 = player1
         self.player2 = player2
 
+    def __cls(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
+
     def start(self):
         Field.draw_field(Field.I)
         while True:
-          
-          player1_move = self.get_move(self.player1)
-          self.field.update_field(player1_move, self.player1.role)
-          self.field.draw_field(self.field.fields)
-          self.result_game()
-            
-          player2_move = self.get_move(self.player2)
-          self.field.update_field(player2_move, self.player2.role)
-          self.field.draw_field(self.field.fields)
-          self.result_game()
+
+            player1_move = self.get_move(self.player1)
+            self.field.update_field(player1_move, self.player1.role)
+            self.__cls()
+            self.field.draw_field(self.field.fields)
+            result = self.result_game()
+            self.check_result(result)
+
+            player2_move = self.get_move(self.player2)
+            self.field.update_field(player2_move, self.player2.role)
+            self.__cls()
+            self.field.draw_field(self.field.fields)
+            result = self.result_game()
+            self.check_result(result)
+
+    def check_result(self, result):
+        if result is False:
+            return
+
+        winner = self.player1.role if result == "X" else self.player2.role
+
+        print(f"Winner is {winner}")
+        exit()
 
     def result_game(self):
-        field = self.field
-        pass
+        fields = self.field.fields
+        if fields[0][0] == fields[1][1] == fields[2][2] == self.player1.role\
+                or fields[0][0] == fields[1][1] == fields[2][2] == self.player2.role:
+            return fields[0][0]
+        elif fields[0][2] == fields[1][1] == fields[2][0] == self.player1.role\
+                or fields[0][2] == fields[1][1] == fields[2][0] == self.player2.role:
+            return fields[0][2]
+
+        if fields[0][0] == fields[1][1] == fields[2][2] == self.player1.role\
+                or fields[0][0] == fields[1][1] == fields[2][2] == self.player2.role:
+            return fields[0][0]
+        elif fields[0][2] == fields[1][1] == fields[2][0] == self.player1.role\
+                or fields[0][2] == fields[1][1] == fields[2][0] == self.player2.role:
+            return fields[0][2]
+
+        if fields[0][0] == fields[1][1] == fields[2][2] == self.player1.role\
+                or fields[0][0] == fields[1][1] == fields[2][2] == self.player2.role:
+            return fields[0][0]
+        elif fields[0][2] == fields[1][1] == fields[2][0] == self.player1.role\
+                or fields[0][2] == fields[1][1] == fields[2][0] == self.player2.role:
+            return fields[0][2]
+
+        elif fields[0][0] == fields[0][1] == fields[0][2] == self.player1.role\
+                or fields[0][0] == fields[0][1] == fields[0][2] == self.player2.role:
+            return fields[0][0]
+
+        elif fields[1][0] == fields[1][1] == fields[1][2] == self.player1.role\
+                or fields[1][0] == fields[1][1] == fields[1][2] == self.player2.role:
+
+            return fields[1][0]
+
+        elif fields[2][0] == fields[2][1] == fields[2][2] == self.player1.role\
+                or fields[2][0] == fields[2][1] == fields[2][2] == self.player2.role:
+
+            return fields[2][0]
+
+        elif fields[0][0] == fields[1][0] == fields[2][0] == self.player1.role\
+                or fields[0][0] == fields[1][0] == fields[2][0] == self.player2.role:
+            return fields[0][0]
+
+        elif fields[0][1] == fields[1][1] == fields[2][1] == self.player1.role\
+                or fields[0][1] == fields[1][1] == fields[2][1] == self.player2.role:
+
+            return fields[0][1]
+
+        elif fields[0][2] == fields[1][2] == fields[2][2] == self.player1.role\
+                or fields[0][2] == fields[1][2] == fields[2][2] == self.player2.role:
+            return fields[0][2]
+
+        else:
+            return False
 
     def get_move(self, player: Player):
         field = self.field
@@ -106,22 +169,8 @@ class Game:
             return self.get_move(player)
 
 
-
-player = Player("🫰", "Ramil")
-player2 = Player("👌", "Huanio")
+player = Player("X", "Ramil")
+player2 = Player("O", "Huanio")
 
 io = Game(player, player2)
 io.start()
-
-# field = Field()
-# player = Player("X")
-# player2 = Player("O")
-
-# while True:
-#     player.new_move(field)
-    
-#     player2.new_move(field)
-
-#     # This is the game
-
-
