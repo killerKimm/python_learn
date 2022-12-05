b77cfa5ecdf3949033d6067802af02addfdd3103
killerKimm <dasistbot@gmail.com>
2022-12-05 14:01:33 +0100

    now score is showing

--- dds.py
+++ dds.py
@@ -46,11 +46,12 @@ class Field:
 
 class Player:
 
-    def __init__(self, role: str, name: str, wins: int = 0, losses: int = 0):
+    def __init__(self, role: str, name: str, wins: int = 0, losses: int = 0, drows: int = 0):
         self.role = role
         self.name = name
         self.wins = wins
         self.losses = losses
+        self.drows = drows
 
     def new_move(self):
         return input(f"{self.name} ({self.role}) move: ")
@@ -62,6 +63,10 @@ class Player:
     def in_case_losses(self):
         self.losses += 1
         return self.losses
+
+    def in_case_drows(self):
+        self.drows += 1
+        return self.drows
 
 
 class Game:
@@ -102,12 +107,23 @@ class Game:
             return
         self.field.clear_field()
         if is_game_end and not result:
+            self.player1.in_case_drows()
+            self.player2.in_case_drows()
             print("Draw!")
+            input("hit enter to continue ")
             return True
 
-        winner = self.player1.role if result == "X" else self.player2.role
+        winner = self.player1.name if result == "X" else self.player2.name
+        # add the winns of losses to the player
+        if result == "X":
+            self.player1.in_case_wins()
+            self.player2.in_case_losses()
 
+        elif result == "O":
+            self.player2.in_case_wins()
+            self.player1.in_case_losses()
         print(f"Winner is {winner}")
+        input("hit enter to continue ")
         return True
 
     def is_game_end(self):
@@ -195,35 +211,53 @@ class Game:
 
 
 class Menu:
+
     print("Hello, bitches")
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
 
         self.field = Field()
 
+    def __cls(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    def get_player_names(self):
+        self.__cls()
+        self.player1 = Player("X", input("\nenter name for player X: "))
+        self.player2 = Player("O", input("\nenter name for player O: "))
+
     def main_menu(self):
 
-        a = input(f"1--PLAY GAME"
-                  "\n2--SHOWE SCORE (not working)"
-                  "\n3--exit:(\n")
-        if a == "1":
-            player1 = Player("X", input("\nenter name for X: "))
-            player2 = Player("O", input("\nenter name for O: "))
+        while True:
+            self.__cls()
+            a = input(f"1--PLAY GAME"
+                      "\n2--SHOWE SCORE"
+                      "\n3--exit:(\n")
 
-            io = Game(player1, player2)
-
-            io.start()
-            self.main_menu()
-        elif a == "2":
-            pass
-
-        elif a == "3":
-            exit()
+            if a == "1":
+                self.__cls()
+                io = Game(self.player1, self.player2)
+                io.start()
+                self.main_menu()
+            elif a == "2":
+                print(f"player {self.player1.name} wins {self.player1.wins} loos {self.player1.losses} drows {self.player1.drows}"
+                      f"\nplayer {self.player2.name} wins {self.player2.wins} loos {self.player2.losses} drows {self.player2.drows}")
+                e = input("\nreturn to menu--1\nor play game--2 \n")
+                if e == "1":
+                    self.main_menu()
+                elif e == "2":
+                    self.__cls()
+                    io = Game(self.player1, self.player2)
+                    io.start()
+                    self.main_menu()
+            elif a == "3":
+                exit()
 
 
 #player = Player("X", "Ramil")
 #player2 = Player("O", "Huanio")
 io = Menu()
+io.get_player_names()
 io.main_menu()
 #io = Game(player, player2)
 # io.start()
